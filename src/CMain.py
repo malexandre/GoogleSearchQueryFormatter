dc65d0d09aaa07e104d9355ecbd6462f5db1c611
Marc ALEXANDRE <marc@malexandre.fr>
2012-06-28 00:05:50 +0200

    Add possibility to call the programm as a script

--- src/CMain.py
+++ src/CMain.py
@@ -5,11 +5,14 @@ Created on 27 juin 2012
 '''
 
 import CParser
+import sys
 
 class CMain:
 
     def __init__(self):
         self.parser = CParser.CParser()
+        
+    def test(self):
         self.simpleTest()
         self.simpleTestWithParanthese()
         self.simpleAnd()
@@ -17,6 +20,9 @@ class CMain:
         self.test1()
         self.test2()
         self.test3()
+        
+    def parse(self, toParse):
+        print(self.parser.parse(toParse))
         
     def simpleTest(self):
         print("'isTrue = 1' =>")
@@ -45,6 +51,18 @@ class CMain:
     def test3(self):
         print("'((isTrue = 1 AND bidule = string) OR (isTrue = false AND bidule = otherString)) AND statement' =>")
         print(self.parser.parse("((isTrue = 1 AND bidule = string) OR (isTrue = false AND bidule = otherString)) AND statement"))
-        
 
-parser = CMain()
+main = CMain()
+
+if (len(sys.argv) > 1):
+    if(sys.argv[1] == "-h" or sys.argv[1] == "-?"):
+        print("Hello World!")
+        print("""Usage : python CMain.py [-h | -? | -test] [string arg]
+                    -h / -? : Display this help
+                    -test : Run the built in test""")
+    elif(sys.argv[1] != "-test"):
+        main.parse(sys.argv[1])
+    else:
+        main.test()
+else:
+    main.test()
